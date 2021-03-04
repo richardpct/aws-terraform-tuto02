0cb5ec9e5f2750e37202b59c28d8b20bda59aa63
richardpct <noreply>
2021-03-04 22:29:44 +0100

    update to Terraform 0.14

--- 02-webserver/main.tf
+++ 02-webserver/main.tf
@@ -1,7 +1,3 @@
-terraform {
-  backend "s3" {}
-}
-
 module "webserver" {
   source = "../modules/webserver"
 
@@ -9,6 +5,6 @@ module "webserver" {
   network_remote_state_bucket = var.bucket
   network_remote_state_key    = var.network_key
   instance_type               = "t2.micro"
-  image_id                    = "ami-0cd23820af84edc85" //Debian Stretch 9.9
+  image_id                    = "ami-0ebc281c20e89ba4b" // Amazon Linux 2018
   ssh_public_key              = var.ssh_public_key
 }
